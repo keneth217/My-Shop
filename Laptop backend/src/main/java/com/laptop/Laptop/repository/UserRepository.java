12b26e12f30feb0e483c8025c63aed055f909197
keneth/217 <kipyegonkeneth03@gmail.com>
2024-09-27 13:05:51 +0300

    add auth to users,generate  products for specific shop,add user to specific shop

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/UserRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/UserRepository.java
@@ -5,16 +5,21 @@ package com.laptop.Laptop.repository;
 import com.laptop.Laptop.entity.User;
 import com.laptop.Laptop.enums.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM User u WHERE u.username = ?1 AND u.shop.id = ?2")
+    Optional<User> findByUsernameAndShopId(String username, Long shopId);
 
-    Optional<User> findByUsernameAndShopId(String username, Long shopId);  // Query users by shopId
-    Optional<User> findByusername(String username);
+   // Optional<User> findByUsernameAndShopId(String username, Long shopId);  // Query users by shopId
+
     User findByRole(Roles role);
 
     Optional<User> findByUsername(String userName);
+
+    Optional<User> findByUsernameAndShopIdAndShopCode(String username, Long shopId, String shopCode);
 }
