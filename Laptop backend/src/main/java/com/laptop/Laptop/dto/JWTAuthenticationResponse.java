e06395b6f9619a3d17951af1008b3e8a45efa358
keneth/217 <kipyegonkeneth03@gmail.com>
2024-09-27 13:04:09 +0300

    add auth to users,generate  products for specific shop,add user to specific shop

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/JWTAuthenticationResponse.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/JWTAuthenticationResponse.java
@@ -5,13 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-
 @NoArgsConstructor
 @AllArgsConstructor
 public class JWTAuthenticationResponse {
     private String token;
     private String username;
     private String role;
-
-
+    private  String shopCode;
+    private Long shopId; // Add the shopId field
 }
