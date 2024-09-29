7b97b3f0a8cf320eb177da141a54822b86b7bae6
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-29 20:32:25 +0300

    add employee,dashboard details,authentications for users details

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/ExpenseRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/ExpenseRepository.java
@@ -1,10 +1,17 @@
 package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Expense;
+import com.laptop.Laptop.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    List<Expense> findByCategory(String category);
+    List<Expense> findByExpenseType(String expenseType);
+
+    List<Expense> findByShop(Shop shop);
+    List<Expense> findByShopIdAndShopCode(Long shopId, String shopCode);
+
+    List<Expense> findByShopAndExpenseType(Shop shop, String name);
 }
