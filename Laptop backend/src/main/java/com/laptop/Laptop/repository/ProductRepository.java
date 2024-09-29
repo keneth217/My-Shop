1abc0802f3a48fe95393a8c3dbddb384ce7bf78b
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-29 20:34:24 +0300

    add employee,dashboard details,authentications for users details

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/ProductRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Product;
+import com.laptop.Laptop.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByShopId(Long shopId);
+    List<Product> findByShop(Shop shop);
 }
