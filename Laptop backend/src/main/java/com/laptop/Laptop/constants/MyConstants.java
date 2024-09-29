573f4dc2d3e9cf7b414973e6cebbc49c0581e131
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-29 20:33:15 +0300

    add employee,dashboard details,authentications for users details

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/constants/MyConstants.java
@@ -0,0 +1,23 @@
+package com.laptop.Laptop.constants;
+
+public class MyConstants {
+
+    private MyConstants() {}
+
+
+    public static final String EMPLOYEE_CREATION = "Employee Added Successfully";
+    public static final String EMPLOYEE_CREATION_CODE = "201";
+
+    public static final String EXPENSE_CREATION = "Expense Added Successfully";
+    public static final String EXPENSE_CREATION_CODE = "201";
+
+    public static final String STOCK_PURCHASE_CREATION = "Stock Added Successfully";
+    public static final String STOCK_PURCHASE_CODE = "201";
+
+    public static final String PRODUCT_SALE_MESSAGE = "Product Sale  Success";
+    public static final String PRODUCT_SALE_CODE = "201";
+    public static final String REGISTER_SHOP_MESSAGE = "Shop Registration Success";
+    public static final String REGISTER_SHOP_CODE = "201";
+    public static final String ERROR_REGISTER_SHOP_MESSAGE = "Shop Registration Failed";
+    public static final String ERROR_REGISTER_SHOP_CODE = "501";
+}
