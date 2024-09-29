36a468cfa5e490b2b8176bc9eb46a39b1880dcf2
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-29 20:35:17 +0300

    add employee,dashboard details,authentications for users details

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/SaleRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/SaleRepository.java
@@ -1,11 +1,17 @@
 package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Sale;
+import com.laptop.Laptop.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
     List<Sale> findByDateBetween(LocalDate startDate, LocalDate endDate);
+    List<Sale> findByShopAndDateBetween(Shop shop, LocalDate startDate, LocalDate endDate);
+    List<Sale> findByShopIdAndShopCode(Long shopId, String shopCode);
+
+    List<Sale> findByShop(Shop shop);
 }
