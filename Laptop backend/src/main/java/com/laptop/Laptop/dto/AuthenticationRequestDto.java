12968efce327ba8c81cd99903c974224abcd7d63
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-29 20:27:53 +0300

    add employee,dashboard details,authentications for users details

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/AuthenticationRequestDto.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/AuthenticationRequestDto.java
@@ -1,8 +1,12 @@
 package com.laptop.Laptop.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequestDto {
     private String userName;
     private String password;
