b228e708bb42bbbb9149fa38a5bcaf3af38aa204
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-29 20:34:15 +0300

    add employee,dashboard details,authentications for users details

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/constants/ProductConstants.java
@@ -0,0 +1,10 @@
+package com.laptop.Laptop.constants;
+
+public class ProductConstants {
+
+    private ProductConstants() {
+    }
+
+    public static  final String PRODUCT_ADDITION="Product created Succesfully";
+    public static  final String PRODUCT_ADDITION_CODE="201";
+}
