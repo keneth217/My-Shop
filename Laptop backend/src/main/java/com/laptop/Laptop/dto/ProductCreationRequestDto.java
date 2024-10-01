d5c06f554cbd6d7b19f182988a12cdb4ec86fd29
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-01 16:48:51 +0300

    fix product multiple images,features,add employee,pay employee an update expenses

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/ProductCreationRequestDto.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/ProductCreationRequestDto.java
@@ -1,12 +1,10 @@
 package com.laptop.Laptop.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -21,7 +19,8 @@ private Long id;
     private double price;  // Selling price
     private double cost;
     private List<String> productFeatures;
-    private List<byte[]> productImages; // Cost price
+    private List<String> productImagesList;
+    private List<MultipartFile> productImages;  // Cost price
     private int stock;
 
 }
