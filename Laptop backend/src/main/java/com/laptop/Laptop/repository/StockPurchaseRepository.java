49d312f57b6475aeab221332485db5169210b168
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-03 17:40:13 +0300

    fix services and pageable data, values in database.

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/StockPurchaseRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/StockPurchaseRepository.java
@@ -1,7 +1,11 @@
 package com.laptop.Laptop.repository;
 
+import com.laptop.Laptop.entity.Shop;
 import com.laptop.Laptop.entity.StockPurchase;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockPurchaseRepository extends JpaRepository<StockPurchase, Long> {
+    Page<StockPurchase> findByShop(Shop shop, Pageable pageable);
 }
