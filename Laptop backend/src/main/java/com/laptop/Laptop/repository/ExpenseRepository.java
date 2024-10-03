0e368a045df48b01843c973dd60a52ceb9672fbd
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-03 17:39:20 +0300

    fix services and pageable data, values in database.

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/ExpenseRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/ExpenseRepository.java
@@ -2,6 +2,8 @@ package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Expense;
 import com.laptop.Laptop.entity.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
@@ -14,4 +16,6 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findByShopIdAndShopCode(Long shopId, String shopCode);
 
     List<Expense> findByShopAndExpenseType(Shop shop, String name);
+
+    Page<Expense> findAllByExpenseType(String name, Pageable pageable);
 }
