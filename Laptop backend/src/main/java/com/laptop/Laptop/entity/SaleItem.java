d16a5845671caec4cc8a4d44aee332d94b941796
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-06 21:07:31 +0300

    generate pdfs for sales,products,shops,purchase

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/entity/SaleItem.java
@@ -0,0 +1,35 @@
+package com.laptop.Laptop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@Builder
+@NoArgsConstructor  // Add this
+@AllArgsConstructor //
+@Entity
+public class SaleItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
+    private Sale sale;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    private double salePrice;
+
+    // Getters and Setters
+}
