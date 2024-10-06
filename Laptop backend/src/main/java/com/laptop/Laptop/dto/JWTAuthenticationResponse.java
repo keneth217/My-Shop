3e5b48321a157ba94bc033186766670c8a561a2d
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-06 21:06:46 +0300

    generate pdfs for sales,products,shops,purchase

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/JWTAuthenticationResponse.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/JWTAuthenticationResponse.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class JWTAuthenticationResponse {
+    private String message;
     private String token;
     private String username;
     private String role;
