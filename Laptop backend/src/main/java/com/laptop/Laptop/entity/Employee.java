49feba3b9085b6b4d0e12b1065db1ba89b57d1a0
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-06 21:03:07 +0300

    generate pdfs for sales,products,shops,purchase

--- Laptop backend/src/main/java/com/laptop/Laptop/entity/Employee.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/entity/Employee.java
@@ -1,11 +1,15 @@
 package com.laptop.Laptop.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor  // Add this
+@AllArgsConstructor //
 @Builder
 @Entity
 public class Employee {
