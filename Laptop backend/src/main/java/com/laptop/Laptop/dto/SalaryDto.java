84b63bf290e4cd317347bb3c378bbd8781ca8afe
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-06 21:07:22 +0300

    generate pdfs for sales,products,shops,purchase

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/SalaryDto.java
@@ -0,0 +1,12 @@
+package com.laptop.Laptop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SalaryDto {
+    private Double SalaryAmount;
+}
