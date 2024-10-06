ac2c12d952fdcccac9c8b84b635b72c28f6ce75a
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-06 21:09:19 +0300

    generate pdfs for sales,products,shops,purchase

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/StockPurchaseRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/StockPurchaseRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockPurchaseRepository extends JpaRepository<StockPurchase, Long> {
     Page<StockPurchase> findByShop(Shop shop, Pageable pageable);
+
+    List<StockPurchase> findByShopIdAndPurchaseDateBetween(Long shopId, LocalDate startDate, LocalDate endDate);
 }
