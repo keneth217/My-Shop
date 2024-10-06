76b5d37829fd24b73ba415fdec0711a1a7000dcf
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-06 21:07:46 +0300

    generate pdfs for sales,products,shops,purchase

--- Laptop backend/src/main/java/com/laptop/Laptop/controller/ShopController.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/controller/ShopController.java
@@ -33,8 +33,8 @@ public class ShopController {
                 .status(HttpStatus.CREATED)
                 .body(new Responsedto(
                         MyConstants.REGISTER_SHOP_CODE,
-                        MyConstants.REGISTER_SHOP_MESSAGE + "\n" +
-                                "Your Unique Shop code is: " + newShop.getShopCode()));
+                        MyConstants.REGISTER_SHOP_MESSAGE + "" + "\n" +
+                                "Your Unique Shop code is: " +"" + newShop.getShopCode()));
     }
         // Activate a shop
     @PostMapping("/{shopId}/activate")
