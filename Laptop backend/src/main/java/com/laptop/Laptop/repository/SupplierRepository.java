485ad9f520b0c054fa9d2d54b5ec31e0b2faac8e
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-07 16:17:35 +0300

    add supliers to stock purchase

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.laptop.Laptop.repository;
+
+import com.laptop.Laptop.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier,Long> {
+}
