68e05bd5b90bd3894c4d5dcd18fe833b6b7ec7f5
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-10 15:59:26 +0300

    pdf receipt

--- Laptop-FrontEnd/src/app/app.config.ts
+++ Laptop-FrontEnd/src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { heroUsers,heroChevronDown,heroChevronUp, heroArrowRightCircle, heroBars4, heroDocument,
+   heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark } from '@ng-icons/heroicons/outline';
 import { routes } from './app.routes';
+import { provideIcons } from '@ng-icons/core';
+import { provideHttpClient } from '@angular/common/http';
+import { Chart, registerables } from 'chart.js';
+import { AngularToastifyModule, ToastService } from 'angular-toastify';
+// Register Chart.js components
+Chart.register(...registerables);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(), // Configure HTTP Client
+    provideIcons({ heroUsers, heroArrowRightCircle, heroBars4,heroChevronDown,heroChevronUp, heroDocument, heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark }),
+    AngularToastifyModule, // Include AngularToastifyModule
+    ToastService // Provide Toast Service
+  ]
 };
