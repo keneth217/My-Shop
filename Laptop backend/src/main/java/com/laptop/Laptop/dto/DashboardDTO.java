f1c3ebb2d9939b8cb71d52706cd2cec7bc729ca7
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-10 13:45:42 +0300

    pdf receipt

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/DashboardDTO.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/DashboardDTO.java
@@ -1,5 +1,6 @@
 package com.laptop.Laptop.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.laptop.Laptop.entity.Product;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +15,9 @@ public class DashboardDTO {
     private double totalExpenses;
     private double grossProfit;
     private double netProfit;
+    @JsonIgnore
     private List<Product> stockAlerts;
+    @JsonIgnore
     private List<Product> topProducts;
     private int totalProducts;
     private long totalUsers;
