81ebdb6b0caeb75b73deeb80827216700c3b97b4
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-11 16:32:23 +0300

    update cart,sale items

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/CartRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/CartRepository.java
@@ -1,10 +1,14 @@
 package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Cart;
+import com.laptop.Laptop.entity.Shop;
 import com.laptop.Laptop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CartRepository {
+public interface CartRepository extends JpaRepository<Cart,Long> {
     Optional<Cart> findByUser(User user);
+
+    Optional<Cart> findByUserAndShop(User user, Shop shop);
 }
