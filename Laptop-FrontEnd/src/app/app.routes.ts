fd38cd52ede4a4a00bbf1b70ad121d8605afd2e7
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-13 19:34:45 +0300

    generate components and add forms modals

--- Laptop-FrontEnd/src/app/app.routes.ts
+++ Laptop-FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SuppliersListComponent } from './suppliers-list/suppliers-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { SalesListComponent } from './sales-list/sales-list.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: "", redirectTo: "login", pathMatch: "full" },
+    { path: 'login', title: 'Login Page', component: LoginComponent },
+    {
+        path: 'dash', title: 'DashBoard', component: DashboardComponent,
+        children: [
+            { path: '', component: AnalyticsComponent },
+            { path: 'product', component: ProductListComponent },
+            { path: 'supplier', component: SuppliersListComponent },
+            { path: 'employee', component: EmployeeListComponent },
+            { path: 'sale', component: SalesListComponent },
+            { path: 'expense', component: ExpenseListComponent },
+        ],
+    },
+    { path: '**', component: PageNotFoundComponent }
+];
