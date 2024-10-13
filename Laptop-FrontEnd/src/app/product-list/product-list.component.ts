03d49cb9ba1240c543b91cf668003dadcdc13f1e
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-13 20:25:07 +0300

    view products componenets

--- Laptop-FrontEnd/src/app/product-list/product-list.component.ts
+++ Laptop-FrontEnd/src/app/product-list/product-list.component.ts
@@ -1,23 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIconsModule } from '@ng-icons/core';
-import { AngularToastifyModule } from 'angular-toastify';
+import { AngularToastifyModule, ToastService } from 'angular-toastify';
 import { AddproductComponent } from "../addproduct/addproduct.component";
 import { AddProductStockComponent } from "../add-product-stock/add-product-stock.component";
+import { ViewSingleProductComponent } from "../view-single-product/view-single-product.component";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [AngularToastifyModule, CommonModule, AngularToastifyModule, NgIconsModule, AddproductComponent, AddProductStockComponent],
+  imports: [AngularToastifyModule, CommonModule, AngularToastifyModule, NgIconsModule, AddproductComponent, AddProductStockComponent, ViewSingleProductComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
+
+
   products: any;
   showAddProduct: Boolean = false;
   showAStockProduct: Boolean = false;
+  showViewProduct:Boolean=false;
 
+
+  constructor(
+ 
+    private toastService: ToastService,
+  ) { }
 
 
 
@@ -34,6 +43,14 @@ export class ProductListComponent {
   closeAddProduct(): void {
     this.showAddProduct = false
   }
+  closeViewProduct() {
+    this.showViewProduct=false;
+  }
+  openViewProduct() {
+  this.showViewProduct=true;
+  }
 
-
+  addToCart() {
+    this.toastService.success("product added to cart succesfully")
+    }
 }
