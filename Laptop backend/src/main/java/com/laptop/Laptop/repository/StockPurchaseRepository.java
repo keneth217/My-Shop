d9d046c8708b2374c8e4b2bb71764f2d26fac971
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-14 13:12:22 +0300

    update sales

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/StockPurchaseRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/StockPurchaseRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 public interface StockPurchaseRepository extends JpaRepository<StockPurchase, Long> {
     Page<StockPurchase> findByShop(Shop shop, Pageable pageable);
 
     List<StockPurchase> findByShopIdAndPurchaseDateBetween(Long shopId, LocalDate startDate, LocalDate endDate);
+
+    List<StockPurchase> findByShopIdAndShopCode(Long shopId, String shopCode);
+
+    List<StockPurchase> findByShop(Shop shop);
 }
