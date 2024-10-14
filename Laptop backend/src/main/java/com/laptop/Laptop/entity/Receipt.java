5faa1740a4dfd522965475c556cd8d9473a924b7
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-14 13:12:33 +0300

    update sales

--- Laptop backend/src/main/java/com/laptop/Laptop/entity/Receipt.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/entity/Receipt.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,26 +17,26 @@ import java.util.List;
 @NoArgsConstructor  // Add this
 @AllArgsConstructor //
 @Entity
-public class ReceiptItem {
+public class Receipt {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String receiptNo;
-    private String receiptDate;
-    private String receiptTo;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private LocalDate receiptDate;
+    private int receiptNo;
+    private String receiptTo;
 
     @ManyToOne
     @JoinColumn(name = "sale_id", nullable = false)
     private Sale sale;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @ManyToOne
     @JoinColumn(name = "shop_id", nullable = false)
