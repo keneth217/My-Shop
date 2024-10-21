3bc23f0ff353af96c7f92b18d463c50f9c0e83e4
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-21 20:45:07 +0300

    fix checkout,reports,receipt generation,correct dtos

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/Responses/Responsedto.java
@@ -0,0 +1,17 @@
+package com.laptop.Laptop.dto.Responses;
+
+import com.laptop.Laptop.dto.ProductDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Responsedto {
+    private String responseCode;
+    private String responseMessage;
+private ProductDto products;
+}
