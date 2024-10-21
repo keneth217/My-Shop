6a0251433227a11809605450556731e60e7d6add
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-21 19:45:20 +0300

    fix checkout,reports,receipt generation

--- Laptop backend/src/main/java/com/laptop/Laptop/services/BusinessService.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/services/BusinessService.java
@@ -10,30 +10,48 @@ import org.springframework.data.domain.Pageable;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+
 public interface BusinessService {
-
     StockPurchase addStockPurchase(Long productId, Long supplierId, StockPurchase stockPurchase);
-    double calculateGrossProfit();
-    double calculateNetProfit();
-    double getTotalSales();
-    double getTotalExpenses();
-
-    Page<Sale> getSalesByDateRange(LocalDate startDate, LocalDate endDate, Pageable pageable);
-    Page<Product> getTopSellingProducts(Pageable pageable);
 
     long totalUsersByShop();
+
     int totalEmployees();
+
     int totalProducts();
-    double getTotalSalesForShop(Long shopId, String shopCode);
-    double calculateNetProfitForShop(Long shopId, String shopCode);
-    double calculateGrossProfitForShop(Long shopId, String shopCode);
-    Page<Product> getStockAlerts(Pageable pageable);
+
+    double calculateGrossProfit();
+
+    double calculateNetProfit();
+
+    public double getTotalSales();
+
+    double getTotalExpenses();
+
     double getTotalInvestments();
 
+    Page<Product> getStockAlerts(Pageable pageable);
+
+    Page<Sale> getSalesByDateRange(LocalDate startDate, LocalDate endDate, Pageable pageable);
+
+    Page<Expense> getExpensesByType(ExpenseType expenseType, Pageable pageable);
+
+//    List<Sale> getSalesForShop(Long shopId, LocalDate startDate, LocalDate endDate);
+//
+//    List<StockPurchase> getStockPurchasesForShop(Long shopId, LocalDate startDate, LocalDate endDate);
 
 
-    List<Sale> getSalesForShop(Long shopId, LocalDate startDate, LocalDate endDate);
-    List<StockPurchase> getStockPurchasesForShop(Long shopId, LocalDate startDate, LocalDate endDate);
+    Page<Product> getTopSellingProducts(Pageable pageable);
 
+    double getTotalSalesForShop(Long shopId, String shopCode);
+
+    double calculateGrossProfitForShop(Long shopId, String shopCode);
+
+    double calculateNetProfitForShop(Long shopId, String shopCode);
+
+
+   List<Sale> getSalesForShop(LocalDate startDate, LocalDate endDate);
+
+    List<StockPurchase> getStockPurchasesForShop(LocalDate startDate, LocalDate endDate) ;
 
 }
