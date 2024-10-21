2982b6bc7e135b23e12b91fbb87b1ee72c52a388
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-21 20:45:32 +0300

    fix checkout,reports,receipt generation,correct dtos

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/AuthenticationRequestDto.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/AuthenticationRequestDto.java
@@ -1,15 +1,16 @@
 package com.laptop.Laptop.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuthenticationRequestDto {
     private String userName;
     private String password;
-   // private Long shopId;
     private String shopCode;
 }
