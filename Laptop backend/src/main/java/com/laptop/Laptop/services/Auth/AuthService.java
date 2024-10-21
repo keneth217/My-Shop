ab83d7f7f2c94a692a872a4dde6d5ab86c86aba4
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-21 20:45:42 +0300

    fix checkout,reports,receipt generation,correct dtos

--- Laptop backend/src/main/java/com/laptop/Laptop/services/Auth/AuthService.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/services/Auth/AuthService.java
@@ -4,16 +4,11 @@ package com.laptop.Laptop.services.Auth;
 
 
 import com.laptop.Laptop.dto.AuthenticationRequestDto;
-import com.laptop.Laptop.dto.JWTAuthenticationResponse;
+import com.laptop.Laptop.dto.Responses.JWTAuthenticationResponse;
 import com.laptop.Laptop.dto.SignUpRequetDto;
 import com.laptop.Laptop.dto.UserUpdateRequestDto;
 import com.laptop.Laptop.entity.User;
-import com.laptop.Laptop.enums.Roles;
-import com.laptop.Laptop.exceptions.CustomerAlreadyExistException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Optional;
 
 public interface AuthService {
     //User createUser(SignUpRequetDto signUpRequest,String adminUsername);
