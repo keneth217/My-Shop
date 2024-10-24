ccd448e798bdd28f210e3310be15f71826eb216d
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-24 14:27:43 +0300

    fix auth response,save token to  local storage,consume apis front end

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/Responses/UserResponse.java
@@ -0,0 +1,17 @@
+package com.laptop.Laptop.dto.Responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+    private String username;
+    private String role;
+    private  String shopCode;
+    private Long shopId;
+}
