2dbc25d5b733ebaf9ceb10b79236296e2b504b1b
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-24 14:16:57 +0300

    fix auth response,save token to  local storage,consume apis front end

--- Laptop backend/src/main/java/com/laptop/Laptop/services/Auth/AuthService.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/services/Auth/AuthService.java
@@ -5,15 +5,16 @@ package com.laptop.Laptop.services.Auth;
 
 import com.laptop.Laptop.dto.AuthenticationRequestDto;
 import com.laptop.Laptop.dto.Responses.JWTAuthenticationResponse;
-import com.laptop.Laptop.dto.SignUpRequetDto;
+import com.laptop.Laptop.dto.SignUpRequestDto;
 import com.laptop.Laptop.dto.UserUpdateRequestDto;
+
 import com.laptop.Laptop.entity.User;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthService {
-    //User createUser(SignUpRequetDto signUpRequest,String adminUsername);
+    //User createUser(SignUpRequestDto signUpRequest,String adminUsername);
 
-    User createUser(SignUpRequetDto signUpRequest) ;
+    User createUser(SignUpRequestDto signUpRequest) ;
 
     User updateUserDetails(UserUpdateRequestDto updateRequest);
 
