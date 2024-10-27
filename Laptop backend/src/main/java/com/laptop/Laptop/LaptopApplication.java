b676f98286fc6a76d88482a4395d446c06329042
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:40:50 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/LaptopApplication.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/LaptopApplication.java
@@ -2,9 +2,11 @@ package com.laptop.Laptop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.laptop.Laptop")
+@EntityScan(basePackages = "com.laptop.Laptop.entity")
 @EnableCaching
 public class LaptopApplication {
 
