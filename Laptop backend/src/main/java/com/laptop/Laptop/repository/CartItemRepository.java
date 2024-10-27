21f9ce99548f1feaefd58b3f95312881f6a5bbd3
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:45:22 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/CartItemRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/CartItemRepository.java
@@ -1,7 +1,8 @@
 package com.laptop.Laptop.repository;
 
-import com.laptop.Laptop.entity.Cart;
+
+import com.laptop.Laptop.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartItemRepository extends JpaRepository<Cart,Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
