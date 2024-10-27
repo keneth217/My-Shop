86cd88c40588ccb9dcd57d10aa96876655efc707
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:37:50 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/SignInRequestDto.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/SignInRequestDto.java
@@ -1,8 +1,13 @@
 package com.laptop.Laptop.dto;
 
-import lombok.Data;
+import lombok.*;
 
+@Builder
 @Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignInRequestDto {
     private String userName;
     private String password;
