722b0038607fc97e67f78abc99d36c8478f39d99
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:41:55 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/ExpenseRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/ExpenseRepository.java
@@ -2,22 +2,30 @@ package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Expense;
 import com.laptop.Laptop.entity.Shop;
+import com.laptop.Laptop.enums.ExpenseType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    List<Expense> findByExpenseType(String expenseType);
+
+    List<Expense> findByType(ExpenseType type); // Use enum directly
 
     List<Expense> findByShop(Shop shop);
+
     List<Expense> findByShopIdAndShopCode(Long shopId, String shopCode);
 
-    List<Expense> findByShopAndExpenseType(Shop shop, String name);
+    List<Expense> findByShopAndType(Shop shop, ExpenseType type); // Use enum directly
 
-    Page<Expense> findAllByExpenseType(String name, Pageable pageable);
+    Page<Expense> findAllByType(Shop shop ,ExpenseType type, Pageable pageable); // Use enum directly
 
-    Page<Expense> findAllByShopAndExpenseType(Shop shop, String name, Pageable pageable);
+    Page<Expense> findAllByShopAndType(Shop shop, ExpenseType type, Pageable pageable); // Use enum directly
+
+    @Query("SELECT SUM(e.amount) FROM Expense e WHERE e.type = :type AND e.shop = :shop")
+    Double sumAmountByTypeAndShop(@Param("type") ExpenseType type, @Param("shop") Shop shop);
 }
