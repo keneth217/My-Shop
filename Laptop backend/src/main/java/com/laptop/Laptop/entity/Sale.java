5a80ce0cf2f3825a0f87a6cc0d592d93888263eb
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:38:16 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/entity/Sale.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/entity/Sale.java
@@ -1,7 +1,9 @@
 package com.laptop.Laptop.entity;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,35 +16,35 @@ import java.util.List;
 
 @Data
 @Builder
-@NoArgsConstructor  // Add this
-@AllArgsConstructor //
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Sale {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate date;
-
     private String shopCode;
     private String salePerson;
     private String customerName;
     private String customerPhone;
     private String customerAddress;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @JsonIgnore
+    @JsonIgnore  // Prevents serialization loop
     private User user;
 
     @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonManagedReference
-    @JsonIgnore
+    @JsonManagedReference  // Helps with bidirectional serialization
+    @JsonIgnore // Optional if not needed in serialization
     private List<SaleItem> saleItems = new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "shop_id", nullable = false)
-    @JsonIgnore
+    @JsonIgnore  // Prevents serialization loop
     private Shop shop;
 
     private double totalPrice;  // Total price for all items in the sale
-
 }
