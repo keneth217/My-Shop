bb96abbb20b5c9c763a5defa568d0c6f9bfe6ce8
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:39:11 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/SaleRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/SaleRepository.java
@@ -2,6 +2,7 @@ package com.laptop.Laptop.repository;
 
 import com.laptop.Laptop.entity.Sale;
 import com.laptop.Laptop.entity.Shop;
+import com.laptop.Laptop.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,4 +21,6 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
     List<Sale> findByShopIdAndDateBetween(Long shopId, LocalDate startDate, LocalDate endDate);
 
     Page<Sale> findAllByShopAndDateBetween(Shop shop, LocalDate startDate, LocalDate endDate, Pageable pageable);
+
+    List<Sale> findByUserAndShop(User loggedInUser, Shop shop);
 }
