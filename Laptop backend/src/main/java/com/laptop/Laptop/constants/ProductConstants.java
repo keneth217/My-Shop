f674dc1c68294c2c6557e3560e043629f89fb82c
keneth217 <kipyegonkeneth03@gmail.com>
2024-10-27 21:38:42 +0300

    fix apis,integrate in frontend

--- Laptop backend/src/main/java/com/laptop/Laptop/constants/ProductConstants.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/constants/ProductConstants.java
@@ -7,4 +7,8 @@ public class ProductConstants {
 
     public static  final String PRODUCT_ADDITION="Product created Succesfully";
     public static  final String PRODUCT_ADDITION_CODE="201";
+    public static  final String PRODUCT_UPDATE="Product updated Succesfully";
+    public static  final String PRODUCT_UPDATE_CODE="201";
+    public static  final String PRODUCT_DELETION="Product deleted Succesfully";
+    public static  final String PRODUCT_DELETION_CODE="201";
 }
