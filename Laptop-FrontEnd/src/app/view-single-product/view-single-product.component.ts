edd9c1c6e3bdf4a5c5ff88ff5f1aa9fc6866fa44
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-28 14:30:43 +0300

    add spring audit trails

--- Laptop-FrontEnd/src/app/view-single-product/view-single-product.component.ts
+++ Laptop-FrontEnd/src/app/view-single-product/view-single-product.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ToastService } from 'angular-toastify';
+import { LoaderService } from '../Services/loader.service';
+import { ProductsService } from '../Services/products.service';
+import { SalesService } from '../Services/sales.service';
 
 @Component({
   selector: 'app-view-single-product',
@@ -10,14 +13,46 @@ import { ToastService } from 'angular-toastify';
 })
 export class ViewSingleProductComponent {
 
+  @Input() productId!: number;
+  product: any[] = [];
   @Output() close = new EventEmitter<void>();
   constructor(
- 
+    private productService: ProductsService, // Inject the ProductService
     private toastService: ToastService,
+    private saleService: SalesService,
+    private loaderService: LoaderService,
+
   ) { }
 
-closeViewForm() {
-  this.close.emit();
-}
+  ngOnInit(): void {
+    this.fetchSingleProduct(); // Call the fetchProducts method on component initialization
+  }
+
+
+  fetchSingleProduct(): void {
+    this.loaderService.show();
+    this.productService.getSingleProduct(this.productId).subscribe({
+      next: (data) => {
+        console.log(data)
+        this.product = data;
+        this.loaderService.hide();// Set the products from the API response
+      },
+      error: (error) => {
+        console.error('Error fetching product:', error);
+        this.loaderService.hide();
+      }
+    });
+  }
+
+  updateProduct() {
+    throw new Error('Method not implemented.');
+    }
+    deleteProduct() {
+    throw new Error('Method not implemented.');
+    }
+
+  closeViewForm() {
+    this.close.emit();
+  }
 
 }
