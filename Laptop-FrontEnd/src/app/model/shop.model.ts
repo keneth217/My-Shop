cf9aa0e9fc42e997cb8599f70ce1b7fe93c11cbb
keneth/217 <kipyegonkeneth03@gmail.com>
2024-10-30 17:12:23 +0300

    fix apis,super user implementations

---
+++ Laptop-FrontEnd/src/app/model/shop.model.ts
@@ -0,0 +1,9 @@
+export interface ShopData {
+  id: number;
+  shopName: string;
+  owner: string;
+  adminUsername: string;
+  phoneNumber: string;
+  address: string;
+  responseMessage: string; // Add this field to match the response structure
+}
