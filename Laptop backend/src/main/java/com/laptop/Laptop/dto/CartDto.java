e595f31696ca12bbf260dd663b15e4435c176b95
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-04 16:42:41 +0300

    fix sale total,claer cat and cart items after sale,generate receipt

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/CartDto.java
@@ -0,0 +1,17 @@
+package com.laptop.Laptop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartDto {
+
+    private int quantity;
+
+
+}
