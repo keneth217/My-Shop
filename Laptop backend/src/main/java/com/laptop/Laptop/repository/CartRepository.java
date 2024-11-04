c404891a6966416ac4c21482b9c7a6c661442022
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-04 16:42:25 +0300

    fix sale total,claer cat and cart items after sale,generate receipt

--- Laptop backend/src/main/java/com/laptop/Laptop/repository/CartRepository.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/repository/CartRepository.java
@@ -3,6 +3,7 @@ package com.laptop.Laptop.repository;
 import com.laptop.Laptop.entity.Cart;
 import com.laptop.Laptop.entity.Shop;
 import com.laptop.Laptop.entity.User;
+import com.laptop.Laptop.enums.CartStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface CartRepository extends JpaRepository<Cart,Long> {
     Optional<Cart> findByUser(User user);
 
     Optional<Cart> findByUserAndShop(User user, Shop shop);
+    Optional<Cart> findByUserAndShopAndStatus(User user, Shop shop, CartStatus status);
+
 }
