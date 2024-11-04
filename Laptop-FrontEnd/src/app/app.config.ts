f9c2e38f03ee8a7f6201998b28104c4860b2d0e7
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-04 16:42:32 +0300

    fix sale total,claer cat and cart items after sale,generate receipt

--- Laptop-FrontEnd/src/app/app.config.ts
+++ Laptop-FrontEnd/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { heroUsers,heroChevronDown,heroChevronUp, heroArrowRightCircle, heroBars4, heroDocument,
    heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark } from '@ng-icons/heroicons/outline';
+
+   import { heroPlaySolid } from '@ng-icons/heroicons/solid';
 import { routes } from './app.routes';
 import { provideIcons } from '@ng-icons/core';
 import { provideHttpClient } from '@angular/common/http';
@@ -15,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(), // Configure HTTP Client
-    provideIcons({ heroUsers, heroArrowRightCircle, heroBars4,heroChevronDown,heroChevronUp, heroDocument, heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark }),
+    provideIcons({ heroUsers, heroArrowRightCircle, heroPlaySolid, heroBars4,heroChevronDown,heroChevronUp, heroDocument, heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark }),
     AngularToastifyModule, // Include AngularToastifyModule
     ToastService // Provide Toast Service
   ]
