a7cdb7b52cfdd4c42324ede9e5065d02d57c15e7
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-05 16:43:31 +0300

    update shop,shop,dtos,fix loading state front end

--- Laptop-FrontEnd/src/app/expense-list/expense-list.component.ts
+++ Laptop-FrontEnd/src/app/expense-list/expense-list.component.ts
@@ -4,17 +4,19 @@ import { AngularToastifyModule, ToastService } from 'angular-toastify';
 import { AddExpenseComponent } from "../add-expense/add-expense.component";
 import { CommonModule } from '@angular/common';
 import { ExpenseService } from '../Services/expense.service';
+import { LoaderComponent } from "../loader/loader.component";
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [AngularToastifyModule, NgIconsModule, AddExpenseComponent,CommonModule],
+  imports: [AngularToastifyModule, NgIconsModule, AddExpenseComponent, CommonModule, LoaderComponent],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css'
 })
 export class ExpenseListComponent {
   expenses:any[]=[];
 showAddExpense: Boolean=false;
+isLoading: boolean=false;
 
 
 constructor(
@@ -27,9 +29,11 @@ ngOnInit(): void {
 }
 
 fetchExpenses(): void {
+  this.isLoading=true;
   this.expenseService.getPExpenses().subscribe({
     next: (data) => {
       this.expenses = data; // Set the products from the API response
+      this.isLoading=false;
     },
     error: (error) => {
       console.error('Error fetching expenses:', error);
