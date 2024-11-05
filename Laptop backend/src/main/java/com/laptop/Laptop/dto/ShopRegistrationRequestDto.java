e5ca081294bb992e89aed9a44b602e179b8cd216
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-05 16:42:43 +0300

    update shop,shop,dtos,fix loading state front end

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/ShopRegistrationRequestDto.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/ShopRegistrationRequestDto.java
@@ -3,8 +3,10 @@ package com.laptop.Laptop.dto;
 import com.laptop.Laptop.enums.ShopStatus;
 import jakarta.validation.constraints.Future;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Setter
@@ -26,4 +28,6 @@ public class ShopRegistrationRequestDto {
     private String shopCode;
     private String phoneNumber;
     private String address;
+    private String shopLogoUrl;
+    private MultipartFile shopLogo;
 }
