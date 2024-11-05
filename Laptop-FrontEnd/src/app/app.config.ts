f51165225ae85f090b984dfe4df4f0a142a393b3
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-05 16:44:16 +0300

    update shop,shop,dtos,fix loading state front end

--- Laptop-FrontEnd/src/app/app.config.ts
+++ Laptop-FrontEnd/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { heroUsers,heroChevronDown,heroChevronUp, heroArrowRightCircle, heroBars4, heroDocument,
-   heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark } from '@ng-icons/heroicons/outline';
+   heroSquares2x2, heroPlusCircle, heroPercentBadge,heroWrenchScrewdriver,heroChartBar, heroCurrencyDollar,heroChatBubbleOvalLeft, heroXMark } from '@ng-icons/heroicons/outline';
 
    import { heroPlaySolid } from '@ng-icons/heroicons/solid';
 import { routes } from './app.routes';
@@ -17,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(), // Configure HTTP Client
-    provideIcons({ heroUsers, heroArrowRightCircle, heroPlaySolid, heroBars4,heroChevronDown,heroChevronUp, heroDocument, heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark }),
+    provideIcons({ heroUsers, heroArrowRightCircle, heroPlaySolid,heroWrenchScrewdriver,heroChartBar,heroChatBubbleOvalLeft, heroBars4,heroChevronDown,heroChevronUp, heroDocument, heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark }),
     AngularToastifyModule, // Include AngularToastifyModule
     ToastService // Provide Toast Service
   ]
