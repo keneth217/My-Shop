1fc97947f995b86ac1ddca69f4b1b6ef7a6be949
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-07 08:12:26 +0300

    user profile,fix images,index tables

--- Laptop backend/src/main/java/com/laptop/Laptop/services/Auth/AuthService.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/services/Auth/AuthService.java
@@ -12,14 +12,20 @@ import com.laptop.Laptop.dto.UserUpdateRequestDto;
 import com.laptop.Laptop.entity.User;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 public interface AuthService {
     //User createUser(SignUpRequestDto signUpRequest,String adminUsername);
 
     User createUser(SignUpRequestDto signUpRequest) ;
 
-    User updateUserDetails(UserUpdateRequestDto updateRequest);
+   // User updateUserDetails(UserUpdateRequestDto updateRequest);
 
 //    Boolean hasCustomerWithPhone(String phone);
    JWTAuthenticationResponse createAuthToken(@RequestBody AuthenticationRequestDto authenticationRequest);
     public JWTAuthenticationResponse createSuperAuthToken(SignInRequestDto sign);
+    UserUpdateRequestDto getLoggedInUserDetails();
+    UserUpdateRequestDto updateUserDetails(UserUpdateRequestDto updateRequest);
+    List<UserUpdateRequestDto> getUsersForLoggedInUserShop();
+    UserUpdateRequestDto updateUserRole(String username, String newRole);
 }
