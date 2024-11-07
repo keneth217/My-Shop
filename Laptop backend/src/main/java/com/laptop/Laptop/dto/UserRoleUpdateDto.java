657fd8619957aaa0cd92815826e6e2d11dd22d99
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-07 08:14:32 +0300

    Add daily,monthly sales reports

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/UserRoleUpdateDto.java
@@ -0,0 +1,15 @@
+package com.laptop.Laptop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRoleUpdateDto {
+
+    private String Role;
+}
