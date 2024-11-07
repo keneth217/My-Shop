8c206f3c7e316a1c7578ccc4dd1d6d65dcb3047b
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-07 08:12:43 +0300

    user profile,fix images,index tables

--- Laptop backend/src/main/java/com/laptop/Laptop/dto/UserUpdateRequestDto.java
+++ Laptop backend/src/main/java/com/laptop/Laptop/dto/UserUpdateRequestDto.java
@@ -1,9 +1,12 @@
 package com.laptop.Laptop.dto;
 
+import com.laptop.Laptop.enums.Roles;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @Setter
+@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,6 +15,13 @@ public class UserUpdateRequestDto {
     private String firstName;
     private  String lastName;
     private String userName;
+    private String shopCode;
+    private String phone;
     private String currentPassword;
     private String newPassword;
+    private Roles role;
+    private MultipartFile userImage;  // Logo file for upload
+    private String UserImageUrl;
+
+
 }
