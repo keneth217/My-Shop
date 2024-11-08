dc3127504d2d27ef0d8f3edc4b3207f86c158a51
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-08 17:30:19 +0300

    generate stock reports,fix receipt size

--- Laptop-FrontEnd/src/app/contactus/contactus.component.ts
+++ Laptop-FrontEnd/src/app/contactus/contactus.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AngularToastifyModule } from 'angular-toastify';
 
 @Component({
   selector: 'app-contactus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,AngularToastifyModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
 export class ContactusComponent {
+contactForm!: FormGroup;
+submitData() {
+throw new Error('Method not implemented.');
+}
 
 }
