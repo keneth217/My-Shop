24ec6d3dc20b0b62e7439b7477cf8d6b0e21a239
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-14 17:11:02 +0300

    send emails

---
+++ Laptop backend/src/main/java/com/laptop/Laptop/emails/EmailServiceImpl.java
@@ -0,0 +1,86 @@
+package com.laptop.Laptop.emails;
+
+import com.laptop.Laptop.dto.EmailDetails;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Objects;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+
+    @Override
+    public void sendEmail(EmailDetails emailDetails) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        } catch (MailException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param emailDetails
+     */
+    @Override
+    public void sendEmailAlertWithAttachment(EmailDetails emailDetails) {
+        File pdfFile = new File("src/main/resources/shop.pdf");
+
+        if (!pdfFile.exists()) {
+            throw new RuntimeException("Attachment file does not exist: " + pdfFile.getAbsolutePath());
+        }
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(senderEmail);
+            mimeMessageHelper.setTo(emailDetails.getRecipient());
+            mimeMessageHelper.setText(emailDetails.getMessageBody());
+            mimeMessageHelper.setSubject(emailDetails.getSubject());
+
+            // Add the attachment
+            FileSystemResource file = new FileSystemResource(pdfFile);
+            mimeMessageHelper.addAttachment(file.getFilename(), file);
+
+            // Check if the file exists before adding it as an attachment
+            if (file.exists()) {
+                mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+                javaMailSender.send(mimeMessage);
+                System.out.println(file.getFilename() + " Shop management PDF sent successfully to " + emailDetails.getRecipient());
+            } else {
+                System.out.println("Attachment file does not exist: " + file);
+            }
+
+        } catch (MessagingException e) {
+            // Log or handle the exception
+            e.printStackTrace();
+            throw new RuntimeException("Error while sending email with attachment", e);
+        }
+    }
+
+
+}
